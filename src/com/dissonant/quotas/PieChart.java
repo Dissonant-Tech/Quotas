57113edb78d424eaa71c1862a2698cdf34adea37
David Rodriguez <dissonant.tech@gmail.com>
2014-09-22 23:00:14 -0400

    Basic PieChart view complete

---
+++ src/com/dissonant/quotas/PieChart.java
@@ -0,0 +1,12 @@
+package com.dissonant.quotas;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class PieChart extends View {
+    public PieChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
