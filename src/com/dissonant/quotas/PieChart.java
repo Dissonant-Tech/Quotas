fa05e5d282acf4d6fd33ff176c348a2cb041fc44
David Rodriguez <dissonant.tech@gmail.com>
2014-09-23 01:12:23 -0400

    PieChart is now it's own class. Project builds

--- src/com/dissonant/quotas/PieChart.java
+++ src/com/dissonant/quotas/PieChart.java
@@ -1,12 +1,36 @@
 package com.dissonant.quotas;
 
 import android.content.Context;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 
 public class PieChart extends View {
+
+    Paint mTextPaint;
+    Paint mPiePaint;
+    Paint mShadowPaint;
+
     public PieChart(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    private void init() {
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setColor(mTextColor);
+        if (mTextHeight == 0) {
+            mTextHeight = mTextPaint.getTextSize();
+        } else {
+            mTextPaint.setTextSize(mTextHeight);
+        }
+        mPiePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPiePaint.setStyle(Paint.Style.FILL);
+        mPiePaint.setTextSize(mTextHeight);
+
+        mShadowPaint = new Paint(0);
+        mShadowPaint.setColor(0xff101010);
+        mShadowPaint.setMaskFilter(new BlurMaskFilter(8, BlurMaskFilter.Blur.NORMAL));
     }
 }
