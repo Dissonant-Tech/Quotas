8d0bd3c038dd107fc65b64c375ae9ca3a8a799d6
David Rodriguez <dissonant.tech@gmail.com>
2014-10-02 08:08:15 -0400

    Basic DB model and helper class finished.

--- app/src/main/java/com/dissonant/quotas/db/QuotaModel.java
+++ app/src/main/java/com/dissonant/quotas/db/QuotaModel.java
@@ -4,68 +4,124 @@ import java.sql.Time;
 
 public class QuotaModel {
 
-	private String title;
-	private boolean isActive;
-	private String description;
-	private Time lengthTime;
-	private Time startTime;
-	private Time endTime;
+    private int id;
+    private String title;
+    private String description;
+    private Time lengthTime;
+    private Time startTime;
+    private Time endTime;
+    private boolean isActive;
 
-    public QuotaModel(String title, boolean isActive, String description, Time lengthTime, Time startTime, Time endTime) { 
-		this.title = title;
-		this.isActive = isActive;
-		this.description = description;
-		this.lengthTime = lengthTime;
-		this.startTime = startTime;
-		this.endTime = endTime;
+    public QuotaModel(int id, String title, String description, 
+            Time lengthTime, Time startTime, Time endTime, boolean isActive) { 
+
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.lengthTime = lengthTime;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.isActive = isActive;
+    }
+    
+    /*
+     * Constructor for SQL 
+     */
+    public QuotaModel(int id, String title, String description, 
+            String lengthTime, String startTime, String endTime, int isActive) { 
+
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.setLengthTime(lengthTime);
+        this.setStartTime(startTime);
+        this.setEndTime(endTime);
+        this.setIsActive(isActive);
     }
 
-	public void setTitle(String title) {
-    	this.title = title;
-	}
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
 
-	public String getTitle() {    
-		return title;
-	}
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setIsActive(boolean isActive) {
-    	this.isActive = isActive;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public boolean getIsActive() {    
-		return isActive;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setDescription(String description) {
-    	this.description = description;
-	}
+    public void setIsActive(int isActive) {
+        if (isActive >= 1) {
+            this.isActive = true;
+        } else {
+            this.isActive = false;
+        }
+    }
 
-	public String getDescription() {    
-		return description;
-	}
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
 
-	public void setLengthTime(Time lengthTime) {
-    	this.lengthTime = lengthTime;
-	}
+    public boolean getIsActive() {    
+        return isActive;
+    }
 
-	public Time getLengthTime() {    
-		return lengthTime;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setStartTime(Time startTime) {
-    	this.startTime = startTime;
-	}
+    public String getDescription() {    
+        return description;
+    }
+    
+    public void setLengthTime(String lengthTime) {
+        Time t = Time.valueOf(lengthTime);
+        this.lengthTime = t;
+    }
 
-	public Time getStartTime() {    
-		return startTime;
-	}
+    public void setLengthTime(Time lengthTime) {
+        this.lengthTime = lengthTime;
+    }
 
-	public void setEndTime(Time endTime) {
-    	this.endTime = endTime;
-	}
+    public Time getLengthTime() {    
+        return lengthTime;
+    }
 
-	public Time getEndTime() {    
-		return endTime;
-	}
+    public void setStartTime(String startTime) {
+        Time t = Time.valueOf(startTime);
+        this.startTime = t;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getStartTime() {    
+        return startTime;
+    }
+    
+    public void setEndTime(String endTime) {
+        Time t = Time.valueOf(endTime);
+        this.endTime = t;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public Time getEndTime() {    
+        return endTime;
+    }
 
 }
--- app/src/main/java/com/dissonant/quotas/db/QuotasSQLiteHelper.java
+++ app/src/main/java/com/dissonant/quotas/db/QuotasSQLiteHelper.java
@@ -1,6 +1,8 @@
 package com.dissonant.quotas.db;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -10,8 +12,8 @@ public class QuotasSQLiteHelper extends SQLiteOpenHelper {
     public static final String TABLE_QUOTAS = "quotas:";
     public static final String COLUMN_ID = "_id";
 
-    public static final String COLUMN_QUOTA_ID = "quota_id";
     public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_LENGTH_TIME = "length_time";
     public static final String COLUMN_START_TIME = "start_time";
     public static final String COLUMN_END_TIME = "end_time";
     public static final String COLUMN_DESCRIPTION = "description";
@@ -19,10 +21,18 @@ public class QuotasSQLiteHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "quotas.db";
     private static final int DATABASE_VERSION = 1;
 
+    private static final String[] COLUMNS = {COLUMN_ID, COLUMN_TITLE,
+        COLUMN_DESCRIPTION, COLUMN_START_TIME, COLUMN_END_TIME};
+
     //Database Creation
     private static final String DATABASE_CREATE = "create table "
         + TABLE_QUOTAS + "(" + COLUMN_ID
-        + " integer primary key autoincrement, " + COLUMN_QUOTA_ID;
+        + " integer primary key autoincrement, "
+        + " text not null " + COLUMN_TITLE 
+        + " text not null " + COLUMN_DESCRIPTION
+        + " text not null " + COLUMN_LENGTH_TIME
+        + " text not null " + COLUMN_START_TIME
+        + " text not null " + COLUMN_END_TIME;
 
     public QuotasSQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -40,5 +50,36 @@ public class QuotasSQLiteHelper extends SQLiteOpenHelper {
                 + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_QUOTAS);
         onCreate(db);
+    }
+
+    public void addQuota(QuotaModel quota) {
+        Log.d("addQouta: ", quota.toString());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TITLE, quota.getTitle());
+        values.put(COLUMN_DESCRIPTION, quota.getDescription());
+        values.put(COLUMN_LENGTH_TIME, quota.getLengthTime().toString());
+        values.put(COLUMN_START_TIME, quota.getStartTime().toString());
+        values.put(COLUMN_END_TIME, quota.getEndTime().toString());
+
+        db.insert(TABLE_QUOTAS, null, values);
+        db.close();
+    }
+
+    public void getQuota(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_QUOTAS, COLUMNS, " id = ?", 
+                new String[] { String.valueOf(id) }, null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        QuotaModel quota = new QuotaModel(cursor.getInt(0), 
+                cursor.getString(1), cursor.getString(2), cursor.getString(3), 
+                cursor.getString(4), cursor.getString(5), cursor.getInt(6));
     }
 }
