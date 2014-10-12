2cabe636407a804d767f9de879ce6a119f5e5a1b
David Rodriguez <dissonant.tech@gmail.com>
2014-10-11 23:58:21 -0400

    Moved model classes. Added QuotaAccess class

--- app/src/main/java/com/dissonant/quotas/db/QuotasSQLiteHelper.java
+++ app/src/main/java/com/dissonant/quotas/db/QuotasSQLiteHelper.java
@@ -24,14 +24,14 @@ public class QuotasSQLiteHelper extends SQLiteOpenHelper {
     public static final String COLUMN_ENDTIME = "end_time";
     public static final String COLUMN_ISACTIVE = "isActive";
 
-    private static final String COLUMNS_REFID = "refId";
-    private static final String COLUMN_TASKDATE = "taskDate";
-    private static final String COLUMN_COMPLETED = "completed";
+    public static final String COLUMNS_REFID = "refId";
+    public static final String COLUMN_TASKDATE = "taskDate";
+    public static final String COLUMN_COMPLETED = "completed";
 
-    private static final String DATABASE_NAME = "quotas.db";
-    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "quotas.db";
+    public static final int DATABASE_VERSION = 1;
 
-    private static final String[] QUOTAS_COLUMNS = {COLUMN_ID, COLUMN_TITLE,
+    public static final String[] QUOTAS_COLUMNS = {COLUMN_ID, COLUMN_TITLE,
         COLUMN_DESCRIPTION, COLUMN_REPEAT, COLUMN_STARTTIME, COLUMN_ENDTIME,
         COLUMN_ISACTIVE
     };
@@ -76,83 +76,7 @@ public class QuotasSQLiteHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public void addQuota(QuotaModel quota) {
-        Log.d("addQouta: ", quota.toString());
-
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_TITLE, quota.getTitle());
-        values.put(COLUMN_DESCRIPTION, quota.getDescription());
-        values.put(COLUMN_REPEAT, quota.getRepeat());
-        values.put(COLUMN_STARTTIME, quota.getStartTime().toString());
-        values.put(COLUMN_ENDTIME, quota.getEndTime().toString());
-        values.put(COLUMN_ISACTIVE, quota.getIsActive());
-
-        db.insert(TABLE_QUOTAS, null, values);
-        db.close();
-    }
-
-    public QuotaModel getQuota(int id) {
-        SQLiteDatabase db = this.getReadableDatabase();
-
-        Cursor cursor = db.query(TABLE_QUOTAS, QUOTAS_COLUMNS, " id = ?",
-                new String[] { String.valueOf(id) }, null, null, null, null);
-
-        if (cursor != null) {
-            cursor.moveToFirst();
-        }
-
-        QuotaModel quota = new QuotaModel(cursor.getInt(0),
-                cursor.getString(1), cursor.getString(2), cursor.getInt(3),
-                cursor.getString(4), cursor.getString(5), cursor.getInt(6));
-
-        Log.d("getQuota("+id+")", quota.toString());
-        return quota;
-    }
-
-    public List<QuotaModel> getAllQuotas() {
-        List<QuotaModel> quotas = new LinkedList<QuotaModel>();
-
-        String query = "SELECT * FROM " + TABLE_QUOTAS;
-
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery(query, null);
-
-        QuotaModel quota = null;
-        if (cursor.moveToFirst()) {
-            do {
-                quota = getQuota(cursor.getInt(0));
-
-                quotas.add(quota);
-            } while (cursor.moveToNext());
-        }
-        return quotas;
-    }
-
-    public int updateQuota(QuotaModel quota) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put("title", quota.getTitle());
-        values.put("description", quota.getDescription());
-
-        int i = db.update(TABLE_QUOTAS,
-                values,
-                COLUMN_ID+" = ?",
-                new String[] {String.valueOf(quota.getId())});
-
-        db.close();
-        return i;
-    }
-
-    public void deleteQuota(QuotaModel quota) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        db.delete(TABLE_QUOTAS,
-                COLUMN_ID+" = ?",
-                new String[] {String.valueOf(quota.getId())});
-
-        db.close();
+    public String[] getQuotaColumns() {
+        return QUOTAS_COLUMNS;
     }
 }
